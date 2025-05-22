b7b820ad64ff957fe3b6bc3700bca78db37aa027
Hoangducc1997 <Hoangducc1997@gmail.com>
2025-05-23 00:56:01 +0700

    New Asset

--- Assets/Scripts/PauseManager.cs
+++ Assets/Scripts/PauseManager.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class PauseManager : MonoBehaviour
+public class PausePanelManager : MonoBehaviour
 {
     [SerializeField] private Button exitButton;
     [SerializeField] private Button pauseButton;
