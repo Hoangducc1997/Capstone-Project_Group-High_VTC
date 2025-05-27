20cf52f5767511950137882be01aecbfd1e7688b
Hoangducc1997 <Hoangducc1997@gmail.com>
2025-05-27 19:44:06 +0700

    Update Map

---
+++ Assets/GameName/Scripts/GameOverManager.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameOverManager : MonoBehaviour
+{
+    [SerializeField] private GameObject gameOverPanel;
+    [SerializeField] private GameObject victoryPanel;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private Button restartGame;
+    [SerializeField] private Button quitGame;
+
+    private void Start()
+    {
+        // Ẩn bảng Game Over khi bắt đầu game
+        gameOverPanel.SetActive(false);
+        victoryPanel.gameObject.SetActive(false);
+        // Gán sự kiện cho nút
+        restartGame.onClick.AddListener(RestartGame);
+        quitGame.onClick.AddListener(QuitGame);
+    }
+
+    // Gọi khi Player chết
+    public void GameOver()
+    {
+        AudioManager.Instance.PlayVFX("GameOver");
+        gameOverPanel.SetActive(true);
+        scoreText.text = "Score: 0"; // Có thể thay bằng điểm thực tế
+
+        // Hiện lại chuột khi game over
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+    public void GameOverWithDelay(float delay)
+    {
+        Invoke("GameOver", delay);
+    }
+
+    public void Victory()
+    {
+        AudioManager.Instance.PlayVFX("Victory");
+        victoryPanel.SetActive(true);
+        scoreText.text = "Score: 0"; // Có thể thay bằng điểm thực tế
+                                     // Hiện lại chuột khi chiến thắng
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    // Restart game
+    public void RestartGame()
+    {
+        AudioManager.Instance.PlayVFX("Click UI");
+        gameOverPanel.SetActive(false);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    // Thoát game
+    public void QuitGame()
+    {
+        AudioManager.Instance.PlayVFX("Click UI");
+        Application.Quit();
+    }
+}
---
+++ Assets/GameName/Scripts/PauseManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PausePanelManager : MonoBehaviour
+{
+    [SerializeField] private Button exitButton;
+    [SerializeField] private Button pauseButton;
+    [SerializeField] private Button resumeButton;
+    [SerializeField] private Button settingButton;
+    [SerializeField] private GameObject pausePanel;
+
+    void Start()
+    {
+        pausePanel.SetActive(false);
+        exitButton.onClick.AddListener(ExitGame);
+        pauseButton.onClick.AddListener(PauseGame);
+        resumeButton.onClick.AddListener(ResumeGame);
+        settingButton.onClick.AddListener(SettingGame);
+    }
+
+    public void ExitGame()
+    {
+        AudioManager.Instance.PlayVFX("Click UI");
+        Application.Quit();
+        Debug.Log("Game has exited."); // Chỉ hoạt động trong Editor
+    }
+
+    public void PauseGame()
+    {
+        AudioManager.Instance.PlayVFX("Click UI");
+        pausePanel.SetActive(true);
+        Time.timeScale = 0f; // Ngừng thời gian trong game
+        Debug.Log("Game Paused.");
+    }
+
+    public void SettingGame()
+    {
+        AudioManager.Instance.PlayVFX("Click UI");
+        Debug.Log("Đã gọi SettingGame");
+        pausePanel.SetActive(false);
+    }
+
+    public void ResumeGame()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1f; // Tiếp tục thời gian trong game
+        Debug.Log("Game Resumed.");
+    }
+}
