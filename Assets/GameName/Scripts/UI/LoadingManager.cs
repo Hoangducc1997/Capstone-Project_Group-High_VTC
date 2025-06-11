29923e13ebdabe6990cef2c0add538695395f8fb
Ngo Hoang Duc <hoangducc1997@gmail.com>
2025-06-11 12:38:09 +0700

    Loading manager

--- Assets/GameName/Scripts/UI/LoadingManager.cs
+++ Assets/GameName/Scripts/UI/LoadingManager.cs
@@ -1,45 +1,72 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
 
 public class LoadingManager : MonoBehaviour
 {
-    public static string NEXT_SCENE = "Menu";
-    [SerializeField] private GameObject processBar;
+    [Header("UI Components")]
+    [SerializeField] private GameObject canvasLoading;
+    [SerializeField] private Image processBar;
     [SerializeField] private TextMeshProUGUI textPercent;
-    [SerializeField] float fixedLoadingTime = 3f;
+    [SerializeField] private TextMeshProUGUI hintText;
+    [SerializeField] private CanvasGroup canvasGroup;
 
+    [Header("Settings")]
+    [SerializeField] private float fixedLoadingTime = 3f;
+    [SerializeField] private List<string> hintMessages;
+    [SerializeField] private float hintChangeInterval = 1f;
     private void Start()
     {
-        StartCoroutine(LoadSceneFixedTime(NEXT_SCENE));
-    }
-    public IEnumerator LoadSceneAsync(string sceneName)
-    {
-        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
-        while (!operation.isDone)
-        {
-            float progress = Mathf.Clamp01(operation.progress / 0.9f);
-            processBar.GetComponent<Image>().fillAmount = progress;
-            textPercent.text = (progress * 100).ToString("0") + "%";
-            yield return null;
-        }
+        StartFakeLoading();
     }
 
-    public IEnumerator LoadSceneFixedTime(string sceneName)
+    public void StartFakeLoading()
     {
-        float elapsedTime = 0f;
-        while(elapsedTime < fixedLoadingTime)
+        StartCoroutine(FakeLoading());
+    }
+
+    private IEnumerator FakeLoading()
+    {
+        canvasLoading.SetActive(true);
+        canvasGroup.alpha = 0f;
+
+        // Fade in
+        while (canvasGroup.alpha < 1f)
         {
-            float process = Mathf.Clamp01(elapsedTime / fixedLoadingTime);
-            processBar.GetComponent<Image>().fillAmount = process;
-            textPercent.text = (process * 100).ToString("0") + "%";
+            canvasGroup.alpha += Time.deltaTime * 2f;
+            yield return null;
+        }
+
+        float elapsedTime = 0f;
+        float nextHintTime = 0f;
+        int hintIndex = 0;
+
+        while (elapsedTime < fixedLoadingTime)
+        {
+            float progress = Mathf.Clamp01(elapsedTime / fixedLoadingTime);
+            processBar.fillAmount = progress;
+            textPercent.text = (progress * 100).ToString("0") + "%";
+
+            if (elapsedTime >= nextHintTime && hintMessages.Count > 0)
+            {
+                hintText.text = hintMessages[hintIndex % hintMessages.Count];
+                hintIndex++;
+                nextHintTime += hintChangeInterval;
+            }
+
             elapsedTime += Time.deltaTime;
             yield return null;
         }
-        SceneManager.LoadScene(sceneName);
-        //AudioManager.Instance.PlayVFX("Click UI");
+
+        // Fade out
+        while (canvasGroup.alpha > 0f)
+        {
+            canvasGroup.alpha -= Time.deltaTime * 2f;
+            yield return null;
+        }
+
+        canvasLoading.SetActive(false);
     }
 }
