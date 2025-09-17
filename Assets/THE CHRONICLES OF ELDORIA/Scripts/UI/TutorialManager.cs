5d5ed7a7e7ea3cb8a7a5b1334cb687876e3a3246
Ngo Hoang Duc <hoangducc1997@gmail.com>
2025-09-17 11:54:30 +0700

    Done/ Tutorial

--- Assets/THE CHRONICLES OF ELDORIA/Scripts/UI/TutorialManager.cs
+++ Assets/THE CHRONICLES OF ELDORIA/Scripts/UI/TutorialManager.cs
@@ -1,22 +1,53 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class TutorialManager : MonoBehaviour
 {
     [SerializeField] private GameObject tutorialPanel;
     [SerializeField] private Button closeTutorialPanel;
+
+    private bool isPlayerInTrigger = false;
+
     private void Start()
     {
         tutorialPanel.SetActive(false);
+        closeTutorialPanel.onClick.AddListener(CloseTutorialPanel);
     }
-    //Khi player cham vao vung tutorial se hien thi panel
+
+    private void Update()
+    {
+        // Nếu player đang trong trigger và nhấn E thì mở panel
+        if (isPlayerInTrigger && Input.GetKeyDown(KeyCode.E))
+        {
+            OpenTutorialPanel();
+        }
+
+        // Nhấn ESC để đóng panel
+        if (tutorialPanel.activeSelf && Input.GetKeyDown(KeyCode.Escape))
+        {
+            CloseTutorialPanel();
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            tutorialPanel.SetActive(true);
-            closeTutorialPanel.onClick.AddListener(CloseTutorialPanel);
+            isPlayerInTrigger = true;
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInTrigger = false;
+        }
+    }
+
+    private void OpenTutorialPanel()
+    {
+        tutorialPanel.SetActive(true);
     }
 
     private void CloseTutorialPanel()
