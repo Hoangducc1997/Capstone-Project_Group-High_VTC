9ac095f4c584e0e1df14e8da05008a125dd9f66c
Ngo Hoang Duc <hoangducc1997@gmail.com>
2025-09-25 13:08:27 +0700

    Update

---
+++ Assets/THE CHRONICLES OF ELDORIA/Scripts/UI/BillboardToPlayer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    private Camera mainCam;
+
+    void Start()
+    {
+        mainCam = Camera.main;
+    }
+
+    void LateUpdate()
+    {
+        if (mainCam == null) return;
+
+        // Chỉ xoay theo trục Y 
+        Vector3 lookPos = mainCam.transform.position - transform.position;
+        lookPos.y = 0; // giữ nguyên chiều đứng
+        if (lookPos.sqrMagnitude > 0.001f)
+        {
+            transform.rotation = Quaternion.LookRotation(-lookPos);
+        }
+    }
+}
